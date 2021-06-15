757329dd381a28c42c317346842bb0a46d3d3e8b
jcordobesMELI <javier.cordobes@mercadolibre.com>
2021-06-15 16:01:01 -0300

    se unifican test de varios US

--- DesafioFinalV1.0/src/main/java/com/example/demo_bootcamp_spring/controller/ApiExceptionControllerAdvice.java
+++ DesafioFinalV1.0/src/main/java/com/example/demo_bootcamp_spring/controller/ApiExceptionControllerAdvice.java
@@ -1,14 +1,18 @@
 package com.example.demo_bootcamp_spring.controller;
 
-import com.example.demo_bootcamp_spring.exceptions.ExistingInboundOrderId;
-import com.example.demo_bootcamp_spring.exceptions.NonExistentProductException;
-import com.example.demo_bootcamp_spring.exceptions.ValidationErrorException;
+import com.example.demo_bootcamp_spring.dtos.ValidationErrorDTO;
+import com.example.demo_bootcamp_spring.exceptions.*;
 import com.example.demo_bootcamp_spring.models.ErrorMessage;
 import com.example.demo_bootcamp_spring.models.ValidationError;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @ControllerAdvice
 public class ApiExceptionControllerAdvice {
@@ -23,9 +27,48 @@ public class ApiExceptionControllerAdvice {
         return new ResponseEntity<ErrorMessage>(errorMessage, HttpStatus.BAD_REQUEST);
     }
 
+    @ExceptionHandler({InvalidSectionId.class})
+    public ResponseEntity<?> handleResponseInvalidSectionId(RuntimeException runtimeException){
+        ErrorMessage errorMessage = new ErrorMessage(runtimeException.getMessage(), HttpStatus.BAD_REQUEST.value());
+        return new ResponseEntity<ErrorMessage>(errorMessage, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler({NoRelatedWarehousesToProduct.class})
+    public ResponseEntity<?> handleNoRelatedWarehousesToProduct(RuntimeException runtimeException){
+        ErrorMessage errorMessage = new ErrorMessage(runtimeException.getMessage(), HttpStatus.BAD_REQUEST.value());
+        return new ResponseEntity<ErrorMessage>(errorMessage, HttpStatus.BAD_REQUEST);
+    }
+    @ExceptionHandler({NotExistingBatch.class})
+    public ResponseEntity<?> handleNotExistingBatch(RuntimeException runtimeException){
+        ErrorMessage errorMessage = new ErrorMessage(runtimeException.getMessage(), HttpStatus.BAD_REQUEST.value());
+        return new ResponseEntity<ErrorMessage>(errorMessage, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler({NotFoundInboundOrderId.class})
+    public ResponseEntity<?> handleNotFoundInboundOrderId(RuntimeException runtimeException){
+        ErrorMessage errorMessage = new ErrorMessage(runtimeException.getMessage(), HttpStatus.BAD_REQUEST.value());
+        return new ResponseEntity<ErrorMessage>(errorMessage, HttpStatus.BAD_REQUEST);
+    }
+
+
+
     @ExceptionHandler(ValidationErrorException.class)
     public ResponseEntity<?> ValidationErrorException(ValidationErrorException validationErrorException){
         ValidationError validationError = new ValidationError(validationErrorException.getField(), validationErrorException.getMessage(), HttpStatus.BAD_REQUEST.value());
         return new ResponseEntity<ValidationError>(validationError, HttpStatus.BAD_REQUEST);
     }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<?> methodArgumentNotValidException(MethodArgumentNotValidException argumentNotValidException) {
+        List<ValidationErrorDTO> valErrorList = new ArrayList<>();
+        for (FieldError error : argumentNotValidException.getFieldErrors()) {
+            ValidationErrorDTO validationErrorDTO = new ValidationErrorDTO();
+            validationErrorDTO.setField(error.getObjectName() + "." + error.getField());
+            validationErrorDTO.setMessage(error.getDefaultMessage());
+            valErrorList.add(validationErrorDTO);
+        }
+        return new ResponseEntity<>(valErrorList, HttpStatus.BAD_REQUEST);
+    }
+
+
 }
--- DesafioFinalV1.0/src/test/java/com/example/demo_bootcamp_spring/services/Batch/BatchServiceTest.java
+++ DesafioFinalV1.0/src/test/java/com/example/demo_bootcamp_spring/services/Batch/BatchServiceTest.java
@@ -1,12 +1,14 @@
 package com.example.demo_bootcamp_spring.services.Batch;
-import com.example.demo_bootcamp_spring.dtos.BatchStockProduct;
-import com.example.demo_bootcamp_spring.dtos.BatchStockProductSearch;
-import com.example.demo_bootcamp_spring.dtos.SearchedWarehouseProducts;
-import com.example.demo_bootcamp_spring.dtos.SectionDTO;
+import com.example.demo_bootcamp_spring.dtos.*;
+import com.example.demo_bootcamp_spring.exceptions.NonExistentProductException;
+import com.example.demo_bootcamp_spring.exceptions.NotExistingBatch;
+import com.example.demo_bootcamp_spring.exceptions.NotFoundInboundOrderId;
 import com.example.demo_bootcamp_spring.models.*;
 import com.example.demo_bootcamp_spring.repository.BatchRepository;
+import com.example.demo_bootcamp_spring.repository.InboundOrderRepository;
+import com.example.demo_bootcamp_spring.repository.ProductsRepository;
 import com.example.demo_bootcamp_spring.repository.SectionRepository;
-import net.bytebuddy.dynamic.DynamicType;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
@@ -28,8 +30,23 @@ class BatchServiceTest {
     BatchRepository batchRepository;
     @Mock
     SectionRepository sectionRepository;
+    @Mock
+    private InboundOrderRepository inboundOrderRepository;
+
     @InjectMocks
     BatchService batchService;
+
+    @Mock
+    private
+    ProductsRepository productsRepository;
+
+    private InboundOrderTransaction inboundOrderTransaction;
+
+    @BeforeEach
+    public void setUp() {
+        inboundOrderTransaction = createInboundOrderTransaction();
+    }
+
 
 
     @Test
@@ -154,6 +171,186 @@ class BatchServiceTest {
         List<BatchStockProduct> listFound = createBatchStockProductOrderByCurrentQuantity();
         return new BatchStockProductSearch(sectionDto,"productTest",listFound);
     }
+
+    //------------------------------------------TESTS METHOD SAVEBATCH--------------------------------------------------
+
+    @Test
+    void whenReceiveOkInboundOrderTransaction_thenSaveBatch() {
+        when(productsRepository.findById(inboundOrderTransaction.getInboundOrder().getBatchStock().get(0).getProductId())).thenReturn(createProduct2());
+        BatchStock batchStock = batchService.saveBatch(inboundOrderTransaction);
+
+        assertEquals("1", batchStock.getBatchStock().get(0).getBatchNumber());
+        assertEquals("1", batchStock.getBatchStock().get(0).getProductId());
+        assertEquals(5, batchStock.getBatchStock().get(0).getInitialQuantity());
+        assertEquals(10, batchStock.getBatchStock().get(0).getCurrentQuantity());
+        assertEquals("2021-05-01", batchStock.getBatchStock().get(0).getManufacturingDate());
+        assertEquals("2021-06-05T10:00", batchStock.getBatchStock().get(0).getManufacturingTime());
+        assertEquals("2021-07-01", batchStock.getBatchStock().get(0).getDueDate());
+        assertEquals(5, batchStock.getBatchStock().get(0).getCurrentTemperature());
+        assertEquals(10, batchStock.getBatchStock().get(0).getMinimumTemperature());
+    }
+
+
+    //TODO hacer saltar excepcion GetExistingInboundOrderId
+//    @Test
+//    void shouldGetExistingInboundOrderId() {
+//
+//        when(inboundOrderRepository.findById(inboundOrderTransaction.getInboundOrder().getOrderNumber()).isPresent()).thenReturn(true);
+//
+//        assertThrows(ExistingInboundOrderId.class, () -> {
+//            batchService.saveBatch(inboundOrderTransaction);
+//        });
+//    }
+
+
+    @Test
+    void shouldGetNonExistentProductException() {
+        Optional<Product> optionalProduct = Optional.empty();
+
+        when(productsRepository.findById(inboundOrderTransaction.getInboundOrder().getBatchStock().get(0).getProductId())).thenReturn(optionalProduct);
+
+        assertThrows(NonExistentProductException.class, () -> {
+            batchService.saveBatch(inboundOrderTransaction);
+        });
+    }
+
+    //------------------------------------------TESTS METHOD PUTBATCH--------------------------------------------------
+    @Test
+
+    void whenReceiveOkInboundOrderTransaction_thenPutBatch() {
+        when(inboundOrderRepository.findById(inboundOrderTransaction.getInboundOrder().getOrderNumber())).thenReturn(createInboundOrder2());
+        when(batchRepository.findByInboundOrder(inboundOrderTransaction.getInboundOrder().getOrderNumber())).thenReturn(createListBatch());
+        BatchStock batchStock = batchService.putBatch(inboundOrderTransaction);
+
+
+        assertEquals("1", batchStock.getBatchStock().get(0).getBatchNumber());
+        assertEquals("1", batchStock.getBatchStock().get(0).getProductId());
+        assertEquals(5, batchStock.getBatchStock().get(0).getInitialQuantity());
+        assertEquals(10, batchStock.getBatchStock().get(0).getCurrentQuantity());
+        assertEquals("2021-05-01", batchStock.getBatchStock().get(0).getManufacturingDate());
+        assertEquals("2021-06-05T10:00", batchStock.getBatchStock().get(0).getManufacturingTime());
+        assertEquals("2021-07-01", batchStock.getBatchStock().get(0).getDueDate());
+        assertEquals(5, batchStock.getBatchStock().get(0).getCurrentTemperature());
+        assertEquals(10, batchStock.getBatchStock().get(0).getMinimumTemperature());
+
+    }
+
+    @Test
+    void shouldGetNotFoundInboundOrderId() {
+        Optional<InboundOrder> optionalInboundOrder = Optional.empty();
+        when(inboundOrderRepository.findById(inboundOrderTransaction.getInboundOrder().getBatchStock().get(0).getProductId())).thenReturn(optionalInboundOrder);
+
+        assertThrows(NotFoundInboundOrderId.class, () -> {
+            batchService.putBatch(inboundOrderTransaction);
+        });
+    }
+
+    @Test
+    void shouldGetNotExistingBatch() {
+        Optional<List<Batch>> optionalBatches = Optional.empty();
+        when(inboundOrderRepository.findById(inboundOrderTransaction.getInboundOrder().getOrderNumber())).thenReturn(createInboundOrder2());
+        when(batchRepository.findByInboundOrder(inboundOrderTransaction.getInboundOrder().getOrderNumber())).thenReturn(optionalBatches);
+
+
+        assertThrows(NotExistingBatch.class, () -> {
+            batchService.putBatch(inboundOrderTransaction);
+        });
+    }
+
+
+
+
+
+    private InboundOrderDTO createInboundOrderDTO() {
+
+        LocalDate date1 = LocalDate.of(2021, 6, 10);
+        List<BatchDTO> batchStock = new ArrayList<>();
+        batchStock.add(createBatchDTO());
+
+        InboundOrderDTO inboundOrderDTO = new InboundOrderDTO();
+        inboundOrderDTO.setOrderDate(date1);
+        inboundOrderDTO.setOrderNumber("1");
+        inboundOrderDTO.setSection(new SectionDTO(State.FF, "1"));
+        inboundOrderDTO.setBatchStock(batchStock);
+
+        return inboundOrderDTO;
+
+    }
+
+
+    private InboundOrderTransaction createInboundOrderTransaction() {
+        return new InboundOrderTransaction(createInboundOrderDTO());
+    }
+
+    private BatchDTO createBatchDTO() {
+        LocalDate manufacturingDate = LocalDate.of(2021, 5, 1);
+        LocalDateTime manufacturingTime = LocalDateTime.of(2021, 6, 5, 10, 00);
+        LocalDate dueDate = LocalDate.of(2021, 7, 1);
+
+        BatchDTO batchDTO = new BatchDTO();
+        batchDTO.setBatchNumber("1");
+        batchDTO.setProductId("1");
+        batchDTO.setCurrentTemperature(5F);
+        batchDTO.setMinimumTemperature(10F);
+        batchDTO.setInitialQuantity(5);
+        batchDTO.setCurrentQuantity(10);
+        batchDTO.setManufacturingDate(manufacturingDate);
+        batchDTO.setManufacturingTime(manufacturingTime);
+        batchDTO.setDueDate(dueDate);
+
+        return batchDTO;
+    }
+
+    public Optional<Product> createProduct2() {
+        Product product = new Product();
+        product.setName("product1");
+        product.setProductId("1");
+        product.setAdditionalInfo("infoAdd");
+        product.setState(State.FF);
+        Optional<Product> optionalProduct = Optional.of(product);
+        return optionalProduct;
+    }
+
+    public Optional<InboundOrder> createInboundOrder2() {
+
+        LocalDate date1 = LocalDate.of(2021, 6, 10);
+        List<BatchDTO> batchStock = new ArrayList<>();
+        batchStock.add(createBatchDTO());
+
+        InboundOrder inboundOrder = new InboundOrder();
+
+        inboundOrder.setOrderDate(date1);
+        inboundOrder.setOrderNumber("1");
+        inboundOrder.setSection(new Section());
+
+        Optional<InboundOrder> optionalInboundOrder = Optional.of(inboundOrder);
+        return optionalInboundOrder;
+    }
+
+    public Optional<List<Batch>> createListBatch() {
+        List<Batch> batches = new ArrayList<>();
+
+        LocalDate manufacturingDate = LocalDate.of(2021, 5, 1);
+        LocalDateTime manufacturingTime = LocalDateTime.of(2021, 6, 5, 10, 00);
+        LocalDate dueDate = LocalDate.of(2021, 7, 1);
+
+        Batch batch = new Batch();
+        batch.setBatchNumber("1");
+        batch.setProduct(createProduct2().get());
+        batch.setCurrentTemperature(5F);
+        batch.setMinimumTemperature(10F);
+        batch.setInitialQuantity(5);
+        batch.setCurrentQuantity(10);
+        batch.setManufacturingDate(manufacturingDate);
+        batch.setManufacturingTime(manufacturingTime);
+        batch.setDueDate(dueDate);
+        batches.add(batch);
+
+        Optional<List<Batch>> optionalBatches = Optional.of(batches);
+
+        return optionalBatches;
+    }
+
 
 
 }
